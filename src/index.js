709512e1d61b484490591719cefa01947ee2f04d
tshuttles <tshuttlesworth@gmail.com>
2020-01-19 20:12:01 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskList = document.getElementById("tasks") 
+  let taskForm = document.getElementById("create-task-form")
+    taskForm.addEventListener('submit', function(e) {
+      e.preventDefault()
+      let userInputField = e.target.querySelector("#new-task-description")
+      let userInputString = userInputField.value 
+      let taskListItem = document.createElement("li")
+      taskListItem.textContent = userInputString
+      taskList.appendChild(taskListItem)
+    });
 });
